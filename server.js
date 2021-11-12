5ddcda882ca7f064e0f16d6120a3d3d69695dfe6
Oliver <oliver.barasinski@gmail.com>
2021-11-12 10:00:48 +0100

    public mappe er fixet + __dirname fixet! - TJEK COOL HTML med COOL CSS!

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 
 import express from "express";
 
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 const app = express()
 
 const port = 3000
 
+const __dirname = fileURLToPath(dirname(import.meta.url));
+
 //app.set('view engine', 'pug')
 
 app.use(express.static('public'));
-
 
 app.get('/', (req, res) => {
     res.sendFile((__dirname) + "/public/HTML/index.html")
