9c53b9a1f1895704e55d0287adc99368a151a7f6
caspersiig <Caspersiig@gmail.com>
2021-11-15 11:58:56 +0100

    server menu med firestore

--- firebase.js
+++ firebase.js
@@ -1,43 +1,3 @@
-
-import express from "express";
-import pug from "pug";
-
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const app = express()
-const port = 3000
-
-//ESSENTIAL -- (type: module) har ikke scopet til den globale variable __dirname
-const __dirname = fileURLToPath(dirname(import.meta.url));
-
-app.set('view engine', 'pug')
-
-app.use(express.static('public'));
-
-
-//HTML
-app.get('/', (req, res) => {
-    res.sendFile((__dirname) + "/public/HTML/index.html")
-});
-
-
-//PUG
-app.get('/pug', (req, res) => {
-  res.render('header', {message: 'CASPER has aine Genitalia ^.^' })
-});
-
-app.get('/pugtest', (req, res) => {
-
-  getMessages().then(list => {
-    res.render('menu', list)
-    console.log(list[0].Børnemenu.Pris)
-  });
-
-});
-
-//**************************************************************************** */
-
 // Import the functions you need from the SDKs you need
 
 import { async } from "@firebase/util";
@@ -81,8 +41,8 @@ const firebaseConfig = {
 //});
 
 // Initialize Firebase
-const app2 = initializeApp(firebaseConfig);
-const db = getFirestore(app2);
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 
 //const analytics = getAnalytics(app);
 
@@ -99,10 +59,4 @@ async function getMessages(){
     return messageList
 }
 //hvad gør vi nu?!
-
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
-
+getMessages().then(list => console.log(list))
