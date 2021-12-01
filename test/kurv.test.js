f2026863c618dedc23330d3a7534b17a1bef5273
Thomas <taulo@live.dk>
2021-12-01 10:59:24 +0100

    Færdiggjorde test i controller:D

--- test/kurv.test.js
+++ test/kurv.test.js
@@ -2,44 +2,26 @@
 //have fun
 //lets gooo!
 
-import {assert} from "chai"
+import {assert, expect} from "chai"
 
 import {cartSum} from "../controller/cartSumControl.js"
-import { quantity } from "../controller/quantityControl.js";
 
-
-// describe("add to numbers", () => {
-//     let one = 1;
-//     let two = 2;
-
-//     let result = one + two;
-
-//     assert.equal(result, 3);
-// })
-// it("should return someting else", () => {
-//     let one = 1;
-//     let two = 3;
-
-//     let result = one + two;
-
-//     assert.equal(result, 5)
-// })
-
-describe("adding two or more items to return the correct price", () => {
+describe("Adding two or more items to return the correct price", () => {
 it ("Should return the total sum of the cart", () => {
     let cart = [{imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 39}]
-    const result = cartSum(cart);
+    const result = cartSum(cart).total;
 
-    assert.equal(result.total, 157);
+    assert.equal(result, 157);
     })
 })
 
-describe("checks the amont of items in the cart", () => {
+describe("Checks the amount of items in the cart", () => {
 it ("Should return the quantity of the cart", () => {
     let cart = [{imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 39}]
-    const result = cartSum(cart);
-
-    assert.equal(result.quantity, 3);   
+    let result = cartSum(cart).quantity;
+    
+    // assert.equal(result.quantity, cart.length);   
+    expect(result).to.be.eq(3);
     })
 })
 
