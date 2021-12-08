82e0fe9e00b384a6fd542f68ee6de079abfa5b98
Oliver <oliver.barasinski@gmail.com>
2021-12-08 11:53:13 +0100

    fucking css

--- controller/cartToString.js
+++ controller/cartToString.js
@@ -2,10 +2,18 @@ export function cartToString(sorted_cart){
 
     let stringify = "";
 
+    let totalSum = 0;
+
     for (const item of sorted_cart) {
+
+        let total = item.pris * item.quantity;
+
+        totalSum += total;
         
         stringify += item.titel + "\t \t \t Antal: " + item.quantity + "\t \t \t Total: " + item.pris * item.quantity + "DKK \n"
     }
+
+    stringify += "\n \t\t\t TOTAL: " + totalSum + "DKK";
 
     return stringify;
 
