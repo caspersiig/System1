395f4f0e8204e1e4266af3da7b7145aec4f9828d
Thomas <taulo@live.dk>
2021-12-09 11:15:54 +0100

    prøver lige igen

--- controller/cartSumControl.js
+++ controller/cartSumControl.js
@@ -1,13 +1,13 @@
-
-
 export function cartSum(cart){
+    if (!Array.isArray(cart)) throw "cart is not an array"
     let total = 0;
     let quantity = cart.length;
 
     for (const item of cart) {
+        if (typeof item.pris === "string") throw "item.pris is a String and should be a number"
+        if (isNaN(item.pris)) throw "item.pris is not a Number"
+        if(item.pris <= 0) throw "item.pris is equal to or less than 0"
         total += item.pris;
     } 
     return {total: total, quantity: quantity}
 }
-
-
--- test/kurv.test.js
+++ test/kurv.test.js
@@ -8,20 +8,27 @@ import {cartSum} from "../controller/cartSumControl.js"
 
 describe("Adding two or more items to return the correct price", () => {
 it ("Should return the total sum of the cart", () => {
-    let cart = [{imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 39}]
-    const result = cartSum(cart).total;
-
-    assert.equal(result, 157);
+        try {
+            let cart = [{imgsrc: "image.png", titel: "titel", pris: undefined}, {imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 39}]
+            const result = cartSum(cart).total;
+            assert.equal(result, 157);
+        } catch (e) {
+            console.log(e)
+        }
     })
 })
 
 describe("Checks the amount of items in the cart", () => {
 it ("Should return the quantity of the cart", () => {
-    let cart = [{imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 39}]
-    let result = cartSum(cart).quantity;
+        try {
+            let cart = [{imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 59}, {imgsrc: "image.png", titel: "titel", pris: 39}]
+            const result = cartSum(cart).quantity;
     
-    // assert.equal(result.quantity, cart.length);   
-    expect(result).to.be.eq(3);
+            // assert.equal(result.quantity, cart.length);   
+            expect(result).to.be.eq(3);
+        } catch (e) {
+            console.log(e)
+        }
     })
 })
 
